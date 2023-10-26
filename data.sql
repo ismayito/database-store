624048eab57177ba5b82c8bfe5feb6c7b0fd1856
Ismail mayito <ismayito1@gmail.com>
2023-10-26 09:56:30 +0300

    Query and update vet clinic database

--- data.sql
+++ data.sql
@@ -15,3 +15,28 @@ VALUES('Charmander', '2020-02-08', 0, true, -11),
     ('Ditto', '2022-05-14', 4, true, 22);
 //insert skipped animal data
 INSERT INTO ANIMALS(NAME,DATE_OF_BIRTH,ESCAPE_ATTEMPTS,NEUTERED,WEIGHT_KG) VALUES('Squirtle','1993-04-02',3,false,-12.13);
+//insert data into owners table;
+INSERT INTO OWNERS(FULL_NAME,AGE)
+ VALUES('Sam Smith',34),
+ ('Jennifer Orwell',19),
+ ('Bob',45),
+ ('Melody Pond',77),
+ ('Dean Winchester',14),
+ ('Jodie Whittaker',38);
+ //insert data into species table;
+ INSERT INTO SPECIES (NAME) VALUES('Pokemon'),('Digimon');
+
+//Modify your inserted animals so it includes the species_id value:
+//If the name ends in "mon" it will be Digimon
+//All other animals are Pokemon
+UPDATE ANIMALS SET species_id=1 WHERE TRIM(NAME) LIKE '%mon';
+//Sam Smith owns Agumon.
+UPDATE ANIMALS SET owner_id =1 WHERE TRIM(NAME)='Agumon';
+//Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE ANIMALS SET owner_id =2 WHERE TRIM(NAME)='Gabumon' OR TRIM(NAME)='Pikachu';
+//Bob owns Devimon and Plantmon.
+UPDATE ANIMALS SET owner_id =3  WHERE TRIM(NAME)='Devimon' OR TRIM(NAME)='Plantmon';
+//Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE ANIMALS SET owner_id =4  WHERE TRIM(NAME)='Charmander' OR TRIM(NAME)='Squirtle' OR TRIM(NAME)='Blossom';
+//Dean Winchester owns Angemon and Boarmon.
+UPDATE ANIMALS SET owner_id =5  WHERE TRIM(NAME)=&apos;Angemon&apos; OR TRIM(NAME)=&apos;Boarmon&apos
--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-ID INT NOT NULL AUTO_INCREMENT,
+ID SERIAL,
 NAME CHAR,
 DATE_OF_bIRTH DATE,
 ESCAPE_ATTEMPTS INT,
@@ -12,4 +12,15 @@ PRIMARY KEY (ID)
 
 // adding a new column species.
 ALTER TABLE ANIMALS ADD COLUMN SPECIES CHAR(20);
+
+//Create table owners;
+CREATE TABLE owners (id SERIAL, full_name CHAR(50),age INT, PRIMARY KEY(ID));
+//Create table species;
+CREATE TABLE species(id SERIAL PRIMARY KEY,name CHAR(50));
+//Remove column species from animals table;
+ ALTER TABLE ANIMALS DROP COLUMN SPECIES;
+//Add column species_id which is a foreign key referencing species table
+ALTER TABLE ANIMALS ADD COLUMN species_id CHAR ,ADD CONSTRAINT Fk_species FOREIGN KEY (species_id) REFERENCES SPECIES(ID);
+//Add column owner_id which is a foreign key referencing the owners table;
+ALTER TABLE ANIMALS ADD COLUMN owner_id INT, ADD CONSTRAINT Fk_owner FOREIGN KEY(owner_id) REFERENCES OWNERS(ID);
 
